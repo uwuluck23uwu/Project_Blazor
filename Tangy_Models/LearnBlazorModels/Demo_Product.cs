63fa9da81a7e4108f0f81a3a7353c25b8096f550
Sumo <144433156+uwuluck23uwu@users.noreply.github.com>
2024-03-26 12:07:58 +0700

    Add project files.

---
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Tangy_Models.LearnBlazorModels
+{
+    public class Demo_Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
