697788ab985689080cc464bc5d6be995a723f3f6
Sumo <144433156+uwuluck23uwu@users.noreply.github.com>
2024-03-30 11:16:34 +0700

    C# Extension

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿using ConsoleApp1;
+
+int Test = 111;
+string test = "Sumo";
+Test.XXXX(999);
+Console.WriteLine(test.Show());
---
+++ ConsoleApp1/TestExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    public static class TestExtension
+    {
+        public static void XXXX(this int x, int num)
+        {
+            Console.WriteLine(x + num);
+        }
+    }
+    public static string Show(this string s, string h)
+    {
+        return $"New message {s} + {h}";
+    }
+}
