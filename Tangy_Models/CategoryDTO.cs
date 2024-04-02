3bfbe0182014ddefa592cc31ab83083158456432
Sumo <144433156+uwuluck23uwu@users.noreply.github.com>
2024-04-02 15:47:34 +0700

    Loading Spinner & Create Category

--- Tangy_Models/CategoryDTO.cs
+++ Tangy_Models/CategoryDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace Tangy_Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tangy_Models
 {
     public class CategoryDTO
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "กรุณากรอกข้อมูล")]
         public string Name { get; set; }
     }
 }
